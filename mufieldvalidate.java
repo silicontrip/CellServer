a36ef4d4b9a056c32cb9500b4adefe8de27c0106
silicontrip <silicontrip@gmail.com>
2019-01-15 16:22:37 +1100

    Added contains and rounding Above Zero methods for UD, and working version of mufieldvalid

--- mufieldvalidate.java
+++ mufieldvalidate.java
@@ -46,58 +46,51 @@ public class mufieldvalidate
 
 		cursor = table.find().iterator();
 
+// loop all fields
 		while (cursor.hasNext()) {
                         Document entitycontent = cursor.next();
                         // arg some of these are strings and some integers...
                         Integer score;
-                        try {
-                                score =  new Integer((String)entitycontent.get("mu"));
-                        } catch (ClassCastException e) {
-                                score =  (Integer)entitycontent.get("mu");
-                        }
+			// I should probably remove this, there is a support script to convert everything to a String
+			// and then find out what's still writing integers
+
+                        //try {
+                              score =  new Integer((String)entitycontent.get("mu"));
+                        //} catch (ClassCastException e) {
+                        //        score =  (Integer)entitycontent.get("mu");
+                        //}
+			// get me the field
                         Document data = (Document) entitycontent.get("data");
                         ArrayList<Document> capturedRegion = (ArrayList<Document>) data.get("points");
 
-                        Document vertexA = (Document) capturedRegion.get(0);
-                        Document vertexB = (Document) capturedRegion.get(1);
-                        Document vertexC = (Document) capturedRegion.get(2);
-                                                        
-                        S2LatLng latlngA = locationToS2 (vertexA);
-                        S2LatLng latlngB = locationToS2 (vertexB);
-                        S2LatLng latlngC = locationToS2 (vertexC);
-
-                        S2PolygonBuilder pb = new S2PolygonBuilder(S2PolygonBuilder.Options.UNDIRECTED_UNION);
-                        pb.addEdge(latlngA.toPoint(),latlngB.toPoint());
-                        pb.addEdge(latlngB.toPoint(),latlngC.toPoint());
-                        pb.addEdge(latlngC.toPoint(),latlngA.toPoint());	
-
-			S2Polygon thisField = pb.assemblePolygon();
+			S2Polygon thisField = cs.getS2Field(capturedRegion);
+			// show me the cells
 			response = cs.cellalize(thisField);	
+
 			UniformDistribution fieldmu = new UniformDistribution(0,0);
-                        double min_mu = 0;
-                        double max_mu = 0;
+
                         for (Iterator<String> id= response.keys(); id.hasNext();)
                         {
                                 String cellid = id.next();
                                 JSONObject cell = response.getJSONObject(cellid);
                                 //System.out.println(cell);
+				// do we have information for this cell
+				// if not we should skip this whole thing
                                 if (cell.has("mu_min"))
                                 {
                                         UniformDistribution cellmu = new UniformDistribution(cell.getDouble("mu_min"),cell.getDouble("mu_max"));
-                                        // System.out.print("" + cellid + ": " + cellmu + " x " + cell.getDouble("area") + " = ");
                                         cellmu = cellmu.mul(cell.getDouble("area"));
-                                        //double cmin_mu = cell.getDouble("area") * cell.getDouble("mu_min");
-                                        //double cmax_mu = cell.getDouble("area") * cell.getDouble("mu_max");
-                                        //System.out.print("field: " + fieldmu + " + ");
-                                        //System.out.println("" + cellmu);
                                         fieldmu = fieldmu.add(cellmu);
-                                        System.out.println(" =  " + fieldmu);
-                                        //min_mu += cmin_mu;
-                                        //max_mu += cmax_mu;
-                                }
+                                } else {
+					break;
+				}
                         }
 
-
+			// validate the score
+			if (!fieldmu.roundAboveZero().contains(score))
+			{
+				System.out.println ("score: " + score + " -> " + fieldmu);
+			}
 
 		}
 	} catch (Exception e) {
