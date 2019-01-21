9b7a79cbe6dc2b32d980fc0df70e0d0f76f2e398
mark heath <mjpeg0@silicontrip.org>
2019-01-21 19:24:00 +1100

    Refactoring to enable field specific debugging.

--- mucelliter.java
+++ mucelliter.java
@@ -8,31 +8,13 @@ public class mucelliter {
 
 	private static HashMap<S2CellId,UniformDistribution> cellStats;
 	private static final double EPSILON = 1e-10;
-
-
-	private static S2LatLng locationToS2 (Document loc) throws NumberFormatException
-	{
-		if (loc.containsKey("latE6") && loc.containsKey("lngE6")) {
-			Integer latE6 = (Integer)loc.get("latE6");
-			Integer lngE6 = (Integer)loc.get("lngE6");
-			Double lat = new Double(latE6) / 1000000.0;
-			Double lng = new Double(lngE6) / 1000000.0;
-
-			return S2LatLng.fromDegrees(lat,lng);
-		}
-
-		throw new NumberFormatException("Object doesn't contain lat/lng");
-	}
-
-	private static S2Polygon polyFromCell (S2Cell cell) 
-	{
-		S2PolygonBuilder pb = new S2PolygonBuilder(S2PolygonBuilder.Options.UNDIRECTED_UNION);
-		pb.addEdge(cell.getVertex(0),cell.getVertex(1));
-		pb.addEdge(cell.getVertex(1),cell.getVertex(2));
-		pb.addEdge(cell.getVertex(2),cell.getVertex(3));
-		pb.addEdge(cell.getVertex(3),cell.getVertex(0));
-		return pb.assemblePolygon();
-	}
+	private CellServer cs;
+	private double range = 0.5;
+	private MongoClient mongo;
+	private MongoDatabase db;
+	private MongoCursor<Document> cursor;
+	private MongoCollection<org.bson.Document> table;
+	private String watchCell;
 
 	private static String doctodt(ArrayList<Document> points)
 	{
@@ -55,21 +37,102 @@ public class mucelliter {
 
 	}
 
-	protected static HashMap<S2CellId,UniformDistribution> process (MongoCursor<Document> cursor, HashMap<S2CellId,UniformDistribution> multi, boolean single,String watchCell)
+	protected HashSet<S2LatLng> fieldHash(ArrayList<Document> f)
 	{
-		double range = 0.5; // some how configurable (1.0 or 0.5)...
+		HashSet<S2LatLng> fieldKey = new HashSet<S2LatLng>();
+
+		//Document vertexA = (Document) f.get(0);
+		Document vertexB = (Document) f.get(1);
+		Document vertexC = (Document) f.get(2);
+							
+		//S2LatLng latlngA = 
+		fieldKey.add(cs.locationToS2 ((Document) f.get(0)));
+		S2LatLng latlngB = cs.locationToS2 (vertexB);
+		S2LatLng latlngC = cs.locationToS2 (vertexC);
+
+
+		//fieldKey.add(latlngA);
+		fieldKey.add(latlngB);
+		fieldKey.add(latlngC);
+
+		return fieldKey;
+		
+	}
+
+	protected UniformDistribution muScore(String i)
+	{
+		//determine upper and lower MU
+		Integer score = new Integer(i);
+		double upper = (score + range);
+		double lower =0.0;
+		if (score > 1) 
+			lower = (score - range);
+
+		return new UniformDistribution(lower,upper);
+	}
+
+	protected UniformDistribution muScore(Document ec)
+	{
+		return muScore((String)ec.get("mu"));
+	}
+
+	protected double getIntArea(S2CellId cello, S2Polygon thisField)
+	{
+		S2Polygon intPoly = new S2Polygon();
+		S2Polygon cellPoly = cs.polyFromCell(new S2Cell(cello));
+		intPoly.initToIntersection(thisField, cellPoly);
+		return intPoly.getArea() * 6367 * 6367 ;
+	}
+
+	protected HashMap<S2CellId,UniformDistribution> processSingle()
+	{
+		// java mongo API doesn't have rewind
+		cursor = table.find().iterator();
+		HashMap<S2CellId,UniformDistribution> multi2 = new HashMap<S2CellId,UniformDistribution>();
+		HashSet<HashSet<S2LatLng>> fieldSet = new HashSet<HashSet<S2LatLng>>();
+
+
+		while (cursor.hasNext()) {
+			Document entitycontent = cursor.next();
+			
+			ArrayList<Document> capturedRegion = (ArrayList<Document>)((Document) entitycontent.get("data")).get("points");
+			HashSet<S2LatLng> fieldKey = fieldHash(capturedRegion);
+			if (!fieldSet.contains(fieldKey))	
+			{
+				fieldSet.add(fieldKey);
+
+				// should validate that the same fields have the same MU
+				S2Polygon thisField = cs.getS2Field(capturedRegion);
+				S2CellUnion cells = cs.getCellsForField (thisField);
+				if ( cells.size() == 1)
+				{
+					Double area = thisField.getArea() * 6367 * 6367 ;
+
+					UniformDistribution score = muScore(entitycontent).div(area);
+					// only 1 cell, is there a simpler way of getting it?
+					for (S2CellId cello: cells) {
+						UniformDistribution cellomu = multi2.get(cello);
+						if (cellomu == null)
+						{
+							cellomu = score;
+						} else {
+							cellomu.refine(score);
+						}
+						multi2.put(cello,cellomu);
+					}	
+				}
+			}
+		}
+		return multi2;	
+	}
+	protected  HashMap<S2CellId,UniformDistribution> processMulti (HashMap<S2CellId,UniformDistribution> multi)
+	{
+		cursor = table.find().iterator();
 
 		HashMap<S2CellId,UniformDistribution> multi2 = new HashMap<S2CellId,UniformDistribution>();
 		HashSet<HashSet<S2LatLng>> fieldSet = new HashSet<HashSet<S2LatLng>>();
 
-		//CellServer cs = new CellServer();
-
-		S2RegionCoverer rc = new S2RegionCoverer();
-			// ingress mu calculation specifics
-		rc.setMaxLevel(13);
-		rc.setMinLevel(0);
-		rc.setMaxCells(20);	
-
+		// copy multiSet over 
 		for (S2CellId cell: multi.keySet())
 		{
 			if (watchCell.equals(cell.toToken()))
@@ -79,59 +142,25 @@ public class mucelliter {
 
 		while (cursor.hasNext()) {
 			Document entitycontent = cursor.next();
-			// arg some of these are strings and some integers...
-			Integer score;
-			try {
-				score =  new Integer((String)entitycontent.get("mu"));
-			} catch (ClassCastException e) {
-				score =  (Integer)entitycontent.get("mu");
-			}
-			Document data = (Document) entitycontent.get("data");
-			ArrayList<Document> capturedRegion = (ArrayList<Document>) data.get("points");
-			Document vertexA = (Document) capturedRegion.get(0);
-			Document vertexB = (Document) capturedRegion.get(1);
-			Document vertexC = (Document) capturedRegion.get(2);
-							
-			S2LatLng latlngA = locationToS2 (vertexA);
-			S2LatLng latlngB = locationToS2 (vertexB);
-			S2LatLng latlngC = locationToS2 (vertexC);
-
-			S2PolygonBuilder pb = new S2PolygonBuilder(S2PolygonBuilder.Options.UNDIRECTED_UNION);
-			pb.addEdge(latlngA.toPoint(),latlngB.toPoint());
-			pb.addEdge(latlngB.toPoint(),latlngC.toPoint());
-			pb.addEdge(latlngC.toPoint(),latlngA.toPoint());
-
-			HashSet<S2LatLng> fieldKey = new HashSet<S2LatLng>();
-
-			fieldKey.add(latlngA);
-			fieldKey.add(latlngB);
-			fieldKey.add(latlngC);
-
-			if (!fieldSet.contains(fieldKey))  { // do not process the same field twice
-				S2Polygon thisField = pb.assemblePolygon();
-
-				Double totalArea = thisField.getArea() * 6367 * 6367 ;
-				HashSet<S2CellId> multiKey = new HashSet<S2CellId>();
-
-				S2CellUnion cells = rc.getCovering (thisField);
+			ArrayList<Document> capturedRegion = (ArrayList<Document>)((Document) entitycontent.get("data")).get("points");
+			HashSet<S2LatLng> fieldKey = fieldHash(capturedRegion);
+			if (!fieldSet.contains(fieldKey))	
+			{
 				fieldSet.add(fieldKey);
+				S2Polygon thisField = cs.getS2Field(capturedRegion);
+				S2CellUnion cells = cs.getCellsForField (thisField);
 
-				if ( (single && cells.size() == 1) || (!single && cells.size() > 1))
+				if (cells.size() > 1)
 				{
+					Double totalArea = thisField.getArea() * 6367 * 6367 ; // only used in watch cell
+					HashSet<S2CellId> multiKey = new HashSet<S2CellId>();
+
+					UniformDistribution  score =  muScore(entitycontent);
 					Double area;
-
-					//determine upper and lower MU
-					double mu1u = (score + range);
-					double mu1l =0.0;
-					if (score > 1) 
-						 mu1l = (score - range);
-
-				
 
 					// loop through field cells
 					for (S2CellId cello: cells) {
 						StringBuilder errmsg = new StringBuilder();
-						UniformDistribution mus = new UniformDistribution(mu1l,mu1u);
 						boolean watchOn = false;
 						if (watchCell.equals(cello.toToken()))
 						{
@@ -141,7 +170,7 @@ public class mucelliter {
 							ArrayList<Document> ent = (ArrayList<Document>) entitycontent.get("ent"); 
 							String cdate = new String("" + ent.get(1)); Date creation= new Date(Long.parseLong(cdate));
 							System.out.println("ts = " +  ent.get(1) + " " + creation);
-							System.out.println("" + cello.toToken() + " : " + mus  + " mu " + mus.div(totalArea) +" mu/km");
+							System.out.println("" + cello.toToken() + " : " + score  + " mu " + score.div(totalArea) +" mu/km");
 
 						}
 					
@@ -150,49 +179,42 @@ public class mucelliter {
 							// if not cell from outer loop
 							if (!cello.toToken().equals(celli.toToken()))
 							{
-								S2Polygon intPoly = new S2Polygon();
-								S2Polygon cellPoly = polyFromCell(new S2Cell(celli));
-								intPoly.initToIntersection(thisField, cellPoly);
-								area = intPoly.getArea() * 6367 * 6367 ;
+								area = getIntArea(celli,thisField);
 
 								// subtract upper range * area from lower MU
 								// subtract lower range * area from upper MU
-							//	errmsg.append (mus +" ");
-							//	errmsg.append (mus.div(totalArea) +" ");
+							//	errmsg.append (score +" ");
+							//	errmsg.append (score.div(totalArea) +" ");
 								UniformDistribution cellmu = multi.get(celli);
 
 								if (cellmu != null)
 								{
-									UniformDistribution cma = cellmu.mul(area);
-									mus = mus.sub(cma);
+									score = score.sub(cellmu.mul(area));
 									if (watchOn)
 									{
-									System.out.print( celli.toToken() );
-									System.out.println(" - " + cma + " (" + cellmu + " x " + area + ") = " + mus);
+										System.out.print( celli.toToken() );
+										System.out.println(" - (" + cellmu + " x " + area + ") = " + score);
 									}
-		
 								}
 								else
 								{
-									mus.setLower(0.0);
+									score.setLower(0.0);
 									if (watchOn)
-										System.out.println("" + celli.toToken() + " undef = " + mus );
+										System.out.println("" + celli.toToken() + " undef = " + score );
 								}
-								
 							}	
 						}
-						S2Polygon intPoly = new S2Polygon();
-						S2Polygon cellPoly = polyFromCell(new S2Cell(cello));
-						intPoly.initToIntersection(thisField, cellPoly);
-						area = intPoly.getArea() * 6367 * 6367 ;
-						mus= mus.div(area);
-						//errmsg.append(" " + mus + " ");
+
+						area = getIntArea(cello,thisField);
+						score= score.div(area);
+
+						//errmsg.append(" " + score + " ");
 						UniformDistribution cellomu = multi2.get(cello);
 						//if (ag.hasOption("v"))
 /*
 						if(watchOn)
 						{
-							System.out.println(cello.toToken() + " : " + cellomu + " x " +  mus);
+							System.out.println(cello.toToken() + " : " + cellomu + " x " +  score);
 
 						}
 */
@@ -201,7 +223,7 @@ public class mucelliter {
 						//update cell
 						if (cellomu == null)
 						{
-							cellomu = mus;
+							cellomu = score;
 							if (watchOn)
 								System.err.println("NEW: " + cello.toToken() + " : " + cellomu);
 						}
@@ -209,7 +231,7 @@ public class mucelliter {
 						{
 							try {
 								UniformDistribution oldcell = new UniformDistribution(cellomu);
-								if (cellomu.refine(mus))
+								if (cellomu.refine(score))
 								{
 									if (watchOn) 
 										System.err.println("UPD: " + cello.toToken() + " : " + cellomu);
@@ -220,8 +242,8 @@ public class mucelliter {
 									System.out.println("analysing: " + entitycontent.get("_id") +" [" + doctodt(capturedRegion) + "]");
 									ArrayList<Document> ent = (ArrayList<Document>) entitycontent.get("ent"); String cdate = new String("" + ent.get(1)); Date creation= new Date(Long.parseLong(cdate));
 									System.out.println("ts = " +  ent.get(1) + " " + creation);
-									System.out.println("" + cello.toToken() + " : " + mus  + " mu " + mus.div(totalArea) +" mu/km");
-									System.out.println(cello.toToken() + " : " + oldcell + " x " +  mus);
+									System.out.println("" + cello.toToken() + " : " + score  + " mu " + score.div(totalArea) +" mu/km");
+									System.out.println(cello.toToken() + " : " + oldcell + " x " +  score);
 									System.out.println(" -> " + cellomu);
 									System.out.println("");
 								}
@@ -255,65 +277,75 @@ public class mucelliter {
 		return multi2;
 	}
 
-	public static void main(String[] args) {
-
-		double range = 0.5;
-		MongoClient mongo;
-		MongoDatabase db;
-		MongoCursor<Document> cursor;
-		MongoCollection<org.bson.Document> table;
-		String watchCell = new String("");
+	public mucelliter () 
+	{
+		range = 0.5;
 		cellStats = new HashMap<S2CellId,UniformDistribution>();
-		HashSet<HashSet<S2LatLng>> fieldSet = new HashSet<HashSet<S2LatLng>>();
-		
-		//HashMap<HashSet<S2CellId>,ArrayList<HashMap<S2CellId,UniformDistribution>>> multiCells = new HashMap<HashSet<S2CellId>,ArrayList<HashMap<S2CellId,UniformDistribution>>>();
+		watchCell = new String("");
 
-		Arguments ag = new Arguments(args);
-
-		if (ag.hasOption("v"))
-			watchCell = ag.getOptionForKey("v");
-			
-
-		System.err.println("starting...");
 		mongo = new MongoClient("localhost", 27017);
                 db = mongo.getDatabase("ingressmu");
 		table = db.getCollection("ingressmu");
 
+		cs = new CellServer();
+	}
+
+
+	public void setWatchCell (String s) { watchCell = s; }
+	public void setRange (double d) { range = d; }
+
+	public static boolean diffMU (HashMap<S2CellId,UniformDistribution> a, HashMap<S2CellId,UniformDistribution> b)
+	{
+		if (a.size() != b.size()) return false;
+		for (S2CellId cell: b.keySet())
+			if (!b.get(cell).equals(a.get(cell)))
+			{
+				System.err.println(a.get(cell) + " -> " + b.get(cell));
+				return true;
+			}
+		return false;
+	}
+
+	public void putMU(S2CellId cell, UniformDistribution mu) { cs.putMU(cell,mu); }
+	public UniformDistribution getMU(S2CellId cell) { return cs.getMU(cell); }
+
+
+	public static void main(String[] args) {
+
+		mucelliter mu = new mucelliter();		
+
+		Arguments ag = new Arguments(args);
+
+		if (ag.hasOption("v"))
+			mu.setWatchCell(ag.getOptionForKey("v"));
+			
+
+		System.err.println("starting...");
 		try {
 
 
 			HashMap<S2CellId,UniformDistribution> multi = new HashMap<S2CellId,UniformDistribution>();
-			HashMap<S2CellId,UniformDistribution> multi2;
-			boolean diff = true;
+			HashMap<S2CellId,UniformDistribution> multi2 = new HashMap<S2CellId,UniformDistribution>();
+			//boolean diff = true;
 				
 			System.err.println("single...");
-			cursor = table.find().iterator();
-			multi = process(cursor,multi,true,watchCell);
 
-			while (diff) {
-				diff = false;
-				System.err.println("iterating...");
-				cursor = table.find().iterator();
-				multi2 = process(cursor,multi,false,watchCell);
-		
-				for (S2CellId cell: multi2.keySet())
-					if (!multi2.get(cell).equals(multi.get(cell)))
-					{
-						//System.err.println(multi.get(cell) + " -> " + multi2.get(cell));
-						diff = true;
-					}
+			multi2 = mu.processSingle();
 
+			while (diffMU(multi,multi2)) {
 				multi = multi2;
+				System.err.println("iterating...");
+				multi2 = mu.processMulti(multi);
+
 			}
 
 
 			boolean first = true;
 
-			CellServer cs = new CellServer();
 			for (S2CellId cell : multi.keySet()) 
 			{
 				UniformDistribution newmu = multi.get(cell);
-				UniformDistribution oldmu = cs.getMU(cell);
+				UniformDistribution oldmu = mu.getMU(cell);
 				if (!newmu.equals(oldmu))
 				{
 					double imp = 1;
@@ -325,7 +357,7 @@ public class mucelliter {
 						S2LatLng cellp = cell.toLatLng();
 						System.out.printf(" https://www.ingress.com/intel?z=15&ll=%f,%f\n",cellp.latDegrees(),cellp.lngDegrees());
 					}
-					cs.putMU(cell, newmu);
+					mu.putMU(cell, newmu);
 				}
 			}
 
